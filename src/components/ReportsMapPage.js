3242f7953df5a9ce2f1b67b317dfd01d7ba68de7
Aayush Kumar <aayush.kumarmail@gmail.com>
2017-04-22 16:41:15 -0400

    added markers

--- src/components/ReportsMapPage.js
+++ src/components/ReportsMapPage.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Components } from 'expo';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+import {
+  MapView
+} from 'expo';
 import {Button, Card, CardSection, Input, Header, Spinner} from './common';
 
 
 
 export default class ReportsMapPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      coordinate1: {
+        latitude: 37.78825,
+        longitude: -122.4324
+      },
+      coordinate2: {
+        latitude: 37.774929,
+        longitude: -122.419416
+      },
+      coordinate3: {
+        latitude: 37.803190,
+        longitude: -122.381832
+      }
+    }
+  }
   render() {
     return (
-      <Components.MapView
+      <MapView
         style={{flex: 1}}
         initialRegion={{
           latitude: 37.78825,
@@ -16,7 +40,20 @@ export default class ReportsMapPage extends React.Component {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <MapView.Marker 
+        coordinate={this.state.coordinate1} 
+        onPress={() => this.props.navigator.pop()}
+        />
+        <MapView.Marker 
+        coordinate={this.state.coordinate2} 
+        onPress={() => this.props.navigator.pop()}
+        />
+        <MapView.Marker 
+        coordinate={this.state.coordinate3} 
+        onPress={() => this.props.navigator.pop()}
+        />
+      </MapView>
     );
   }
 }
